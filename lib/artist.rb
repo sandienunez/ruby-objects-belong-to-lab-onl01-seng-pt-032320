0fbbd1b62a5fd5c6ee2e38740a5e2a124333f299
Sandie Nunez <snunez@vt.edu>
2020-04-20 17:36:33 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,7 +1,19 @@
+require "pry"
 class Artist
   attr_accessor :song, :name
+  def initialize(name)
+    @name = name 
+  end 
   
+  def genre 
+    @genre 
+  end 
   
+  def genre=(genre)
+    self.genre = genre 
+  end 
+    
+  binding.pry
 end 
 
 #artist = Artist.new("Beyonce")
